c0c8be23ac1706543a160c897b58b715b9a3029a
theertha301995 <theerthasurendran235@gmail.com>
2025-12-25 23:57:43 +0530

    committed

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import './index.css'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
